5d30d2d48e5c8fc4af61701a222d20460a89fba2
Artem Grigoryan <artem.grigoryan1994@gmail.com>
2018-02-20 13:58:09 +0300

    FEAT: csv export

--- app/csv.cpp
+++ app/csv.cpp
@@ -1,9 +1,18 @@
 #include "csv.h"
 
-
 CSV::CSV(const QString &fileName) :
-    fileName(fileName)
-{}
+    file(fileName)
+{
+    file.open(QIODevice::ReadWrite | QIODevice::Text);
+    stream = new QTextStream(&file);
+    stream->setCodec("UTF-8");
+}
+
+CSV::~CSV()
+{
+    delete stream;
+    file.close();
+}
 
 void CSV::read(Model *model) const
 {
@@ -19,5 +28,47 @@ Model CSV::read() const
 
 void CSV::write(const Model *model)
 {
-    // TODO
+    writeSchema(model->schema);
+    writeRows(model->rows);
+}
+
+
+void CSV::writeSchema(const Model::Schema &schema)
+{
+    QStringList line;
+    for (const QString &column : schema) {
+        line << encodeValue(column);
+    }
+
+    *stream << line.join(',') << '\n';
+}
+
+void CSV::writeRows(const Model::Rows &rows)
+{
+    for (const Model::Row &row : rows) {
+        writeRow(row);
+    }
+}
+
+void CSV::writeRow(const Model::Row &row)
+{
+    QStringList line;
+    for (const QVariant &value : row) {
+        line << encodeValue(value);
+    }
+
+    *stream << line.join(',') << '\n';
+}
+
+
+QString CSV::encodeValue(const QVariant &value)
+{
+    QString str = value.toString();
+
+    // Экранируем строку содержащую специальные символы
+    if (str.indexOf(',') != -1 || str.indexOf('\n') != -1 || str.indexOf('"') != -1) {
+        return "\"" + QString(str).replace('"', "\"\"") + "\"";
+    }
+
+    return str;
 }
--- app/csv.h
+++ app/csv.h
@@ -1,7 +1,12 @@
 #ifndef CSV_H
 #define CSV_H
 
+#include <QFile>
+#include <QIODevice>
 #include <QString>
+#include <QStringList>
+#include <QTextStream>
+#include <QVariant>
 
 #include "model.h"
 
@@ -9,13 +14,21 @@ class CSV
 {
 public:
     CSV(const QString &fileName);
+    ~CSV();
 
     void read(Model *model) const;
     Model read() const;
     void write(const Model *model);
 
 private:
-    QString fileName;
+    QFile file;
+    QTextStream *stream;
+
+    void writeSchema(const Model::Schema &schema);
+    void writeRows(const Model::Rows &rows);
+    void writeRow(const Model::Row &row);
+
+    QString encodeValue(const QVariant &value);
 };
 
 #endif // CSV_H
